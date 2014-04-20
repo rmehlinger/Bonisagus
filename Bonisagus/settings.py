4c4e73290ac8de9effe39147563a7152fbc1d2a1
rmehlinger <rmehlinger@gmail.com>
2014-04-20 12:09:35 -0700

    fix settings.py, runtime version

--- Bonisagus/settings.py
+++ Bonisagus/settings.py
@@ -87,7 +87,7 @@ MONGO_CLIENT = 'mongodb://bonisagus:Fh0JBCP2N1qYGPDrM500@oceanic.mongohq.com:100
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
