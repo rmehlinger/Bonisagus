a71243a699bdf0024190118f460caa121c0eb080
Richard Mehlinger <rmehlinger@gmail.com>
2016-03-16 23:14:35 -0700

    Herokify

--- Core/console_middleware.py
+++
@@ -1,7 +0,0 @@
-class ConsoleExceptionMiddleware:
-    def process_exception(self, request, exception):
-        import traceback
-        import sys
-        exc_info = sys.exc_info()
-        print(''.join(traceback.format_exception(*(exc_info or sys.exc_info()))), file=sys.stderr)
-        #print repr(request)
